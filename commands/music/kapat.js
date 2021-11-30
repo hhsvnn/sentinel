230c140e48126fc970c3542903fe17eb99cd90f6
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 20:46:38 +0300

    Update kapat.js

--- commands/music/kapat.js
+++ commands/music/kapat.js
@@ -3,15 +3,15 @@ module.exports = {
     aliases: ['dc',"kapat"],
 
     execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
+        if (!message.member.voice.channel) return message.channel.send(`<:mavitik:864814562313240576> Bir Sesli Kanala Bağlı Değilsin!`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576>  Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`**${client.emotes.error} - Şuanda Çalan Bir Müzik Yok!**`);
+        if (!client.player.getQueue(message)) return message.channel.send(`<:mavitik:864814562313240576>  Şuanda Çalan Bir Müzik Yok!`);
 
         client.player.setRepeatMode(message, false);
         const success = client.player.stop(message);
 
-        if (success) message.channel.send(`**${client.emotes.success} - Müzik Çalma Sistemi Durduruldu!**`);
+        if (success) message.channel.send(`<:pause:844301372610052148> Müzik Çalma Sistemi Durduruldu!`);
     },
 };
