91234dd0354b2f4ec1c8ef6412684d52bd45103a
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 20:58:20 +0300

    Update error.js

--- player/error.js
+++ player/error.js
@@ -1,16 +1,16 @@
 module.exports = (client, error, message, ...args) => {
     switch (error) {
         case 'NotPlaying':
-            message.channel.send(`**${client.emotes.error} - Bu Sunucuda Çalınan Müzik Yok!**`);
+            message.channel.send(`<:mavitik:864814562313240576> Bu Sunucuda Çalınan Müzik Yok!`);
             break;
         case 'NotConnected':
-            message.channel.send(`**${client.emotes.error} - Herhangi Bir Ses Kanalına Bağlı Değilsiniz!**`);
+            message.channel.send(`<:mavitik:864814562313240576> Herhangi Bir Ses Kanalına Bağlı Değilsiniz!`);
             break;
         case 'UnableToJoin':
-            message.channel.send(`**${client.emotes.error} - Ses Kanalınıza Katılamıyorum, Lütfen İzinlerimi Kontrol Edin!**`);
+            message.channel.send(`<:mavitik:864814562313240576> Ses Kanalınıza Katılamıyorum, Lütfen İzinlerimi Kontrol Edin!`);
             break;
         case 'VideoUnavailable':
-            message.channel.send(`**${client.emotes.error} - ${args[0].title} İsimli Müzik Bulunamadı!**`);
+            message.channel.send(`<:mavitik:864814562313240576> ${args[0].title} İsimli Müzik Bulunamadı!`);
             break;
         case 'MusicStarting':
             message.channel.send(`**Müzik Başlıyor Lütfen Bekleyin!**`);
