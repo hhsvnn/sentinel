26597325e4a54c22d93c50f905a4a5f60c004e3c
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 20:57:34 +0300

    Update channelEmpty.js

--- player/channelEmpty.js
+++ player/channelEmpty.js
@@ -1,3 +1,3 @@
 module.exports = (client, message, queue) => {
-    message.channel.send(`**${client.emotes.error} - Bulunduğum Sesli Kanalda Kimse Bulunmadığı İçin Kanaldan Ayrıldım ve Müzik Durdu!**`);
+    message.channel.send(`<:leave:915300439065497651> Bulunduğum Sesli Kanalda Kimse Bulunmadığı İçin Kanaldan Ayrıldım ve Müzik Durdu!`);
 };
