bdcc29bdea4be65cf4c97c69854ffddfec69e1dc
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 20:37:27 +0300

    Update durdur.js

--- commands/music/durdur.js
+++ commands/music/durdur.js
@@ -3,16 +3,16 @@ module.exports = {
     aliases: ["durdur","dur","stop"],
 
     execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
+        if (!message.member.voice.channel) return message.channel.send(`<:mavitik:864814562313240576> Bir Sesli Kanala Bağlı Değilsin!`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576> Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`**${client.emotes.error} - Şuanda Bir Müzik Çalmıyor!**`);
+        if (!client.player.getQueue(message)) return message.channel.send(`<:mavitik:864814562313240576>Şuanda Bir Müzik Çalmıyor!`);
 
-        if (client.player.getQueue(message).paused) return message.channel.send(`**${client.emotes.error} - Müzik Çalma Sistemi Zaten Durdurulmuş!**`);
+        if (client.player.getQueue(message).paused) return message.channel.send(`<:mavitik:864814562313240576> Müzik Çalma Sistemi Zaten Durdurulmuş!`);
 
         const success = client.player.pause(message);
 
-        if (success) message.channel.send(`**${client.emotes.success} - \`${client.player.getQueue(message).playing.title}\` İsimli Şarkı Durduruldu!**`);
+        if (success) message.channel.send(`<:pause:844301372610052148> \`${client.player.getQueue(message).playing.title}\` İsimli Şarkı Durduruldu!`);
     },
 };
