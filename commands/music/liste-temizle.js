c0dea76cf699678502a125b3baa464268e975a93
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 20:49:00 +0300

    Update liste-temizle.js

--- commands/music/liste-temizle.js
+++ commands/music/liste-temizle.js
@@ -3,16 +3,16 @@ module.exports = {
     aliases: ['cq',"liste-sil","listetemizle"],
 
     execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
+        if (!message.member.voice.channel) return message.channel.send(`<:mavitik:864814562313240576>- Bir Sesli Kanala Bağlı Değilsin!`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576> Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`**${client.emotes.error} - Şuanda Bir Müzik Çalmıyor!**`);
+        if (!client.player.getQueue(message)) return message.channel.send(`<:mavitik:864814562313240576> Şuanda Bir Müzik Çalmıyor!`);
 
-        if (client.player.getQueue(message).tracks.length <= 1) return message.channel.send(`**${client.emotes.error} - Müzik Çalma Sırasında Sadece Bir Şarkı Var!**`);
+        if (client.player.getQueue(message).tracks.length <= 1) return message.channel.send(`<a:kilit:850623406499037194> Müzik Çalma Sırasında Sadece Bir Şarkı Var!`);
 
         client.player.clearQueue(message);
 
-        message.channel.send(`**${client.emotes.success} - Müzik Çalma Sırası Kaldırıldı!**`);
+        message.channel.send(`<a:kilit:850623406499037194>Müzik Çalma Sırası Kaldırıldı!`);
     },
 };
