2bde892cd803d7b6c6d2ef67f15d5657c2e3ef5c
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 20:51:28 +0300

    Update ses.js

--- commands/music/ses.js
+++ commands/music/ses.js
@@ -3,18 +3,18 @@ module.exports = {
     aliases: ["ses"],
 
     execute(client, message, args) {
-        if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
+        if (!message.member.voice.channel) return message.channel.send(`<:mavitik:864814562313240576> Bir Sesli Kanala Bağlı Değilsin!`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576> Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`**${client.emotes.error} - Şu Anda Çalan Bir Müzik Yok!**`);
+        if (!client.player.getQueue(message)) return message.channel.send(`<:mavitik:864814562313240576> Şu Anda Çalan Bir Müzik Yok!`);
 
-        if (!args[0] || isNaN(args[0]) || args[0] === 'Infinity') return message.channel.send(`**${client.emotes.error} - Lütfen Geçerli Bir Sayı Gir!**`);
+        if (!args[0] || isNaN(args[0]) || args[0] === 'Infinity') return message.channel.send(`<:mavitik:864814562313240576> Lütfen Geçerli Bir Sayı Gir!`);
 
-        if (Math.round(parseInt(args[0])) < 1 || Math.round(parseInt(args[0])) > 250) return message.channel.send(`**${client.emotes.error} - Lütfen Geçerli Bir Sayı Gir \`(1 - 250)\`**`);
+        if (Math.round(parseInt(args[0])) < 1 || Math.round(parseInt(args[0])) > 250) return message.channel.send(`<a:voice:915298814703833129> Lütfen Geçerli Bir Sayı Gir \`(1 - 250)\``);
 
         const success = client.player.setVolume(message, parseInt(args[0]));
 
-        if (success) message.channel.send(`**${client.emotes.success} - Müzik Sesi \`%${parseInt(args[0])}\` Olarak Ayarlandı!**`);
+        if (success) message.channel.send(`<a:voice:915298814703833129> Müzik Sesi \`%${parseInt(args[0])}\` Olarak Ayarlandı!`);
     },
 };
