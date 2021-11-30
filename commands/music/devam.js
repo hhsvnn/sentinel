1a6c73ce5f9453ff2cdb2201448352364534bd1a
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 18:24:57 +0300

    Update devam.js

--- commands/music/devam.js
+++ commands/music/devam.js
@@ -3,16 +3,16 @@ module.exports = {
     aliases: ["devam"],
 
     execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
+        if (!message.member.voice.channel) return message.channel.send(`<:mavitik:864814562313240576> Bir Sesli Kanala Bağlı Değilsin*`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576> Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`**${client.emotes.error} - Şu Anda Çalan Bir Müzik Yok!**`);
+        if (!client.player.getQueue(message)) return message.channel.send(`<:mavitik:864814562313240576> Şu Anda Çalan Bir Müzik Yok!`);
 
-        if (!client.player.getQueue(message).paused) return message.channel.send(`**${client.emotes.error} - Müzik Zaten Çalmaya Devam Ediyor!**`);
+        if (!client.player.getQueue(message).paused) return message.channel.send(`<:mavitik:864814562313240576> Müzik Zaten Çalmaya Devam Ediyor!`);
 
         const success = client.player.resume(message);
 
-        if (success) message.channel.send(`**${client.emotes.success} - \`${client.player.getQueue(message).playing.title}\` İsimli Şarkı Devam Ediyor!**`);
+        if (success) message.channel.send(`<:play:844300167163019324> \`${client.player.getQueue(message).playing.title}\` İsimli Şarkı Devam Ediyor!`);
     },
 };
