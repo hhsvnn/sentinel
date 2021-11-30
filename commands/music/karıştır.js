04c093da26622974beef851ce8ed5195592df998
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 20:47:59 +0300

    Update karıştır.js

--- commands/music/karıştır.js
+++ commands/music/karıştır.js
@@ -3,14 +3,14 @@ module.exports = {
     aliases: ['sh',"karıştır"],
 
     execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
+        if (!message.member.voice.channel) return message.channel.send(`<:mavitik:864814562313240576> Bir Sesli Kanala Bağlı Değilsin!`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576> Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`**${client.emotes.error} - Şuanda Çalan Bir Müzik Yok!**`);
+        if (!client.player.getQueue(message)) return message.channel.send(`<:mavitik:864814562313240576> Şuanda Çalan Bir Müzik Yok!`);
 
         const success = client.player.shuffle(message);
 
-        if (success) message.channel.send(`**${client.emotes.success} - Çalma Listesi Karıştırıldı Şarkı Sırası Değişti | \`${client.player.getQueue(message).tracks.length}\` Toplam Şarkı!**`);
+        if (success) message.channel.send(`<a:music:844299514076856321> Çalma Listesi Karıştırıldı Şarkı Sırası Değişti | \`${client.player.getQueue(message).tracks.length}\` Toplam Şarkı!`);
     },
 };
