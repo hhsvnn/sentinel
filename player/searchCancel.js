68d017dfbb622246d09b6ba7148b876812d00700
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 21:00:21 +0300

    Update searchCancel.js

--- player/searchCancel.js
+++ player/searchCancel.js
@@ -1,3 +1,3 @@
 module.exports = (client, message, query, tracks) => {
-    message.channel.send(`**${client.emotes.error} - Lütfen Çalmasını İstediğin Müziğin Sıra Numarasını Yaz | Müzik Seçmediğin İçin Komutu Tekrar Kullanman Gerekli!**`);
+    message.channel.send(`<a:voice:915298814703833129> Lütfen Çalmasını İstediğin Müziğin Sıra Numarasını Yaz | Müzik Seçmediğin İçin Komutu Tekrar Kullanman Gerekli!`);
 };
