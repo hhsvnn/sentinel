635189feb1b4c14a50d83f1111ab4b7307d78b12
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 20:44:58 +0300

    Update filtre.js

--- commands/music/filtre.js
+++ commands/music/filtre.js
@@ -3,17 +3,17 @@ module.exports = {
     aliases: ["filtre", "efekt"],
 
     execute(client, message, args) {
-        if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
+        if (!message.member.voice.channel) return message.channel.send(`<:mavitik:864814562313240576> Bir Sesli Kanala Bağlı Değilsin!`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576> Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`**${client.emotes.error} - Şuanda Bir Müzik Çalmıyor!**`);
+        if (!client.player.getQueue(message)) return message.channel.send(`<:mavitik:864814562313240576> Şuanda Bir Müzik Çalmıyor!`);
 
-        if (!args[0]) return message.channel.send(`**${client.emotes.error} - Lütfen Geçerli Bir Efekt Filtresi İsmi Yaz!**`);
+        if (!args[0]) return message.channel.send(`<:search:844300676036820992> Lütfen Geçerli Bir Efekt Filtresi İsmi Yaz!`);
 
         const filterToUpdate = client.filters.find((x) => x.toLowerCase() === args[0].toLowerCase());
 
-        if (!filterToUpdate) return message.channel.send(`**${client.emotes.error} - Böyle Bir Efekt Filtresi Yok!**`);
+        if (!filterToUpdate) return message.channel.send(`<:mavitik:864814562313240576> Böyle Bir Efekt Filtresi Yok!`);
 
         const filtersUpdated = {};
 
@@ -21,7 +21,7 @@ module.exports = {
 
         client.player.setFilters(message, filtersUpdated);
 
-        if (filtersUpdated[filterToUpdate]) message.channel.send(`**${client.emotes.music} - Filtreyi Müziğe Ekliyorum, Lütfen Bekleyin... Not: \`Müzik Ne Kadar Uzunsa, O Kadar Uzun Sürecektir!\`**`);
-        else message.channel.send(`**${client.emotes.music} - Müzikteki Filtreyi Devre Dışı Bırakıyorum, Lütfen Bekleyin!**`);
+        if (filtersUpdated[filterToUpdate]) message.channel.send(`<a:ghostheart:859090821645991956>Filtreyi Müziğe Ekliyorum, Lütfen Bekleyin... Not: \`Müzik Ne Kadar Uzunsa, O Kadar Uzun Sürecektir!\``);
+        else message.channel.send(`<a:ghostheart:859090821645991956> Müzikteki Filtreyi Devre Dışı Bırakıyorum, Lütfen Bekleyin!`);
     },
 };
