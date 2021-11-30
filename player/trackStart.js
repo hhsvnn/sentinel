aa3be82ee3523495aca260dcfabd1c01184df679
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 21:01:36 +0300

    Update trackStart.js

--- player/trackStart.js
+++ player/trackStart.js
@@ -1,3 +1,3 @@
 module.exports = (client, message, track) => {
-    message.channel.send(`**${client.emotes.music} - Çalmaya Başladı \`${track.title}\` | Bağlanılan Kanal: <#${message.member.voice.channel.id}>**`);
+    message.channel.send(`<:play:844300167163019324> Çalmaya Başladı \`${track.title}\` | Bağlanılan Kanal: <#${message.member.voice.channel.id}>`);
 };
