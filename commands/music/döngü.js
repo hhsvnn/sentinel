5b322367d007e9e7fb7092b8ce333d6bddfd098a
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 20:39:57 +0300

    Update döngü.js

--- commands/music/döngü.js
+++ commands/music/döngü.js
@@ -1,29 +1,29 @@
 module.exports = {
     name: 'loop',
-    aliases: ['lp', 'repeat','döngü'],
+    aliases: ['lp', 'loop','döngü'],
 
     execute(client, message, args) {
-        if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
+        if (!message.member.voice.channel) return message.channel.send(`<:mavitik:864814562313240576> Bir Sesli Kanala Bağlı Değilsin!`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576> Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`**${client.emotes.error} - Şuanda Bir Müzik Çalmıyor!**`);
+        if (!client.player.getQueue(message)) return message.channel.send(`<:mavitik:864814562313240576> Şuanda Bir Müzik Çalmıyor!`);
 
         if (args.join(" ").toLowerCase() === 'liste') {
             if (client.player.getQueue(message).loopMode) {
                 client.player.setLoopMode(message, false);
-                return message.channel.send(`**${client.emotes.success} - Müzik Döngü Modu Kapatıldı!**`);
+                return message.channel.send(`<:play:844300167163019324> Müzik Döngü Modu Kapatıldı!`);
             } else {
                 client.player.setLoopMode(message, true);
-                return message.channel.send(`**${client.emotes.success} - Müzik Döngü Modu Aktif Listedeki Tüm Müzikler Durmadan Tekrarlanacak!**`);
+                return message.channel.send(`<a:kilit:850623406499037194> - Müzik Döngü Modu Aktif Listedeki Tüm Müzikler Durmadan Tekrarlanacak!`);
             };
         } else {
             if (client.player.getQueue(message).repeatMode) {
                 client.player.setRepeatMode(message, false);
-                return message.channel.send(`**${client.emotes.success} - Müzik Döngü Modu Kapatıldı!**`);
+                return message.channel.send(`<:play:844300167163019324> Müzik Döngü Modu Kapatıldı!`);
             } else {
                 client.player.setRepeatMode(message, true);
-                return message.channel.send(`**${client.emotes.success} - Müzik Döngü Modu Aktif Bu Müzik Durmadan Tekrarlanacak!**`);
+                return message.channel.send(`<a:kilit:850623406499037194>  Müzik Döngü Modu Aktif Bu Müzik Durmadan Tekrarlanacak!`);
             };
         };
     },
