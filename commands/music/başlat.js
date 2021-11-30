554089ef145b35b80e27d600f54c371ea4ce02c7
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 18:23:07 +0300

    Update başlat.js

--- commands/music/başlat.js
+++ commands/music/başlat.js
@@ -3,11 +3,11 @@ module.exports = {
     aliases: ['p',"başla","çal"],
 
     execute(client, message, args) {
-        if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
+        if (!message.member.voice.channel) return message.channel.send(`<:mavitik:864814562313240576> Bir Sesli Kanala Bağlı Değilsin!`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576> Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!args[0]) return message.channel.send(`**${client.emotes.error} - Lütfen Bir Şarkı İsmi Yaz!**`);
+        if (!args[0]) return message.channel.send(`<:mavitik:864814562313240576> Lütfen Bir Şarkı İsmi Yaz!`);
 
         client.player.play(message, args.join(" "), { firstResult: true });
     },
