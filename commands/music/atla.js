ec63efc92815291eca20241cfe6b717491eec9b0
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 18:22:20 +0300

    Update atla.js

--- commands/music/atla.js
+++ commands/music/atla.js
@@ -5,12 +5,12 @@ module.exports = {
     execute(client, message) {
         if (!message.member.voice.channel) return message.channel.send(`**${client.emotes.error} - Bir Sesli Kanala Bağlı Değilsin!**`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`**${client.emotes.error} - Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!**`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`<:mavitik:864814562313240576> Bulunduğun Sesli Kanala Giriş Yapmaya Yetkim Yok!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`**${client.emotes.error} - Şuanda Çalan Bir Müzik Yok!**`);
+        if (!client.player.getQueue(message)) return message.channel.send(`<:mavitik:864814562313240576> Şuanda Çalan Bir Müzik Yok!`);
 
         const success = client.player.skip(message);
 
-        if (success) message.channel.send(`**${client.emotes.success} - Çalan Müzik Değiştirildi ve Sıradaki Diğer Müziğe Geçildi!**`);
+        if (success) message.channel.send(`<:ileri:844300887232479303>  Çalan Müzik Değiştirildi ve Sıradaki Diğer Müziğe Geçildi!`);
     },
 };
