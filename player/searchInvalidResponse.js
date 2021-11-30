5b9149a6143b171e812a87588becbaabcb7187fd
theloxd <77835704+theloxd@users.noreply.github.com>
2021-11-30 21:00:51 +0300

    Update searchInvalidResponse.js

--- player/searchInvalidResponse.js
+++ player/searchInvalidResponse.js
@@ -1,6 +1,6 @@
 module.exports = (client, message, query, tracks, content, collector) => {
     if (content === 'cancel') {
         collector.stop();
-        return message.channel.send(`**${client.emotes.success} - Müzik Seçimi İptal Edildi!**`);
-    } else message.channel.send(`**${client.emotes.error} - Geçerli Bir Sıra Numarası Göndermeniz Gerekli! | \`1 - ${tracks.length}\`**`);
+        return message.channel.send(`<a:voice:915298814703833129> Müzik Seçimi İptal Edildi!`);
+    } else message.channel.send(`<a:voice:915298814703833129> Geçerli Bir Sıra Numarası Göndermeniz Gerekli! | \`1 - ${tracks.length}\``);
 };
